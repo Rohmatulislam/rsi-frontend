d8cf09c6f4244c9cb3ae42f8c1a48ee4c30cb317
rohmatulislam <rohmatulislam084@gmail.com>
2025-12-31 00:28:20 +0800

    fix: add edge runtime to main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import HomePage from "~/features/home/pages/HomePage";
 
+export const runtime = 'edge';
+
 export default async function Page(props: any) {
     // Standardize access to params for both old and new Next.js
     const params = await props.params;
